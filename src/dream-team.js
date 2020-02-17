819c253a0b25780f51657bce3c6450d0072d72c1
Aleksey Ivanov <aleksey.vl.ivanov@gmail.com>
2020-02-17 23:08:31 +1000

    feat: add "Dream team" solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,10 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+
+  return members
+    .filter(e => typeof e === 'string')
+    .map(e => e.trim())
+    .map(e => e[0].toUpperCase())
+    .sort()
+    .join('');
 };
