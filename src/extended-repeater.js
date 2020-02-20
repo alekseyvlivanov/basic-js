2dccea1ad4a926988dba1f66273181bfc7f20153
Aleksey Ivanov <aleksey.vl.ivanov@gmail.com>
2020-02-20 19:12:38 +1000

    feat: add "Extended repeater" solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,21 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  if (typeof str !== 'string' || !(str instanceof String)) {
+    str = String(str);
+  }
+
+  let {
+    repeatTimes: rT = 1,
+    separator: s = '+',
+    addition: a = '',
+    additionRepeatTimes: aRT = 1,
+    additionSeparator: aS = '|',
+  } = options;
+
+  if (typeof a !== 'string' || !(a instanceof String)) {
+    a = String(a);
+  }
+
+  const additionString = new Array(aRT).fill(a).join(aS);
+
+  return new Array(rT).fill(`${str}${additionString}`).join(s);
 };
-  
